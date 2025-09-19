0624c593494535b1f6f9399ed3fa3b98ce29cb50
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:41:17 +0500

    Update 24.object_arrays_and_arguments.cpp

--- 24.object_arrays_and_arguments.cpp
+++ 24.object_arrays_and_arguments.cpp
@@ -1,22 +1,32 @@
+// This program demonstrates:
+// 1. Creating an array of objects
+// 2. Passing objects as function arguments
+// 3. Comparing object data
+
 #include <iostream>
 using namespace std;
 
+// =========================
+// Class Definition
+// =========================
 class Student {
 private:
-    int id;
-    string name;
+    int id;         // private member: student ID
+    string name;    // private member: student name
 
 public:
+    // Function to set student data
     void setData(int i, string n) {
         id = i;
         name = n;
     }
 
+    // Function to display student data
     void display() {
         cout << "ID: " << id << ", Name: " << name << endl;
     }
 
-    // Function taking object as argument
+    // Function that takes another Student object to compare IDs
     void compare(Student s) {
         if (id == s.id)
             cout << "Student " << name << " has the same ID as " << s.name << endl;
@@ -26,23 +36,29 @@ public:
 };
 
 int main() {
-    // Array of objects
-    Student students[3];
 
-    // Assign data
+    // =========================
+    // Array of objects
+    // =========================
+    Student students[3]; // array to store 3 Student objects
+
+    // Assign data to each student
     students[0].setData(1, "Umar");
     students[1].setData(2, "Ali");
     students[2].setData(1, "Sara");
 
     // Display all students
+    cout << "Student Details:\n";
     for (int i = 0; i < 3; i++) {
         students[i].display();
     }
 
-    // Passing object as argument
+    // =========================
+    // Compare objects
+    // =========================
     cout << "\nComparing Students:\n";
     students[0].compare(students[2]); // compare Umar with Sara
     students[0].compare(students[1]); // compare Umar with Ali
 
-    return 0;
+    return 0; // end of program
 }
