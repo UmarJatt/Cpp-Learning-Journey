5c58ec4ec864433b1878a0bcf258539a1bc0b729
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:45:29 +0500

    Update 27.more_on_friend_functions.cpp

--- 27.more_on_friend_functions.cpp
+++ 27.more_on_friend_functions.cpp
@@ -1,36 +1,47 @@
+/*
+Program: Friend Functions and Operator Overloading in C++
+Description:
+This program demonstrates the use of friend functions in C++. 
+1. A friend function can access private members of a class even though it is not a member function.
+2. Operator overloading allows us to define custom behavior for operators like << for user-defined classes.
+3. The program also shows how a single friend function can access private members of two different classes at the same time (Square and Rectangle).
+*/
+
 #include <iostream>
 using namespace std;
 
+// =========================
+// Complex Class
+// =========================
 class Complex {
 private:
-    int real, imag;
+    int real, imag;  // private data members
 
 public:
-    Complex(int r = 0, int i = 0) {
-        real = r;
-        imag = i;
-    }
+    Complex(int r = 0, int i = 0) { real = r; imag = i; }
 
-    // Declare friend function for addition
+    // Friend function to add two Complex numbers
     friend Complex add(Complex c1, Complex c2);
 
-    // Friend function for operator overloading
+    // Friend function to overload << operator for printing
     friend ostream& operator<<(ostream &out, Complex &c);
 };
 
-// Friend function to add two Complex numbers
+// Add two Complex numbers
 Complex add(Complex c1, Complex c2) {
     return Complex(c1.real + c2.real, c1.imag + c2.imag);
 }
 
-// Friend function to overload << operator
+// Overload << to print Complex numbers
 ostream& operator<<(ostream &out, Complex &c) {
     out << c.real << " + " << c.imag << "i";
     return out;
 }
 
-// Friend function with two different classes
-class Rectangle; // forward declaration
+// =========================
+// Square and Rectangle Classes
+// =========================
+class Rectangle; // forward declaration for friend function
 
 class Square {
 private:
@@ -39,7 +50,7 @@ private:
 public:
     Square(int s) { side = s; }
 
-    // Friend function for comparing areas
+    // Friend function for comparing areas with Rectangle
     friend void compareArea(Square s, Rectangle r);
 };
 
@@ -48,10 +59,7 @@ private:
     int length, width;
 
 public:
-    Rectangle(int l, int w) {
-        length = l;
-        width = w;
-    }
+    Rectangle(int l, int w) { length = l; width = w; }
 
     // Friend function shared with Square
     friend void compareArea(Square s, Rectangle r);
@@ -63,21 +71,23 @@ void compareArea(Square s, Rectangle r) {
     int areaRectangle = r.length * r.width;
 
     if (areaSquare > areaRectangle)
-        cout << "Square has larger area.\n";
+        cout << "Square has larger area." << endl;
     else if (areaSquare < areaRectangle)
-        cout << "Rectangle has larger area.\n";
+        cout << "Rectangle has larger area." << endl;
     else
-        cout << "Both have equal area.\n";
+        cout << "Both have equal area." << endl;
 }
 
+// =========================
+// Main Function
+// =========================
 int main() {
     // Example 1: Adding Complex numbers
     Complex c1(3, 4), c2(1, 2);
     Complex sum = add(c1, c2);
-
     cout << "Sum of Complex numbers: " << sum << endl;
 
-    // Example 2: Comparing Square and Rectangle areas
+    // Example 2: Comparing areas of Square and Rectangle
     Square s(5);
     Rectangle r(4, 6);
     compareArea(s, r);
