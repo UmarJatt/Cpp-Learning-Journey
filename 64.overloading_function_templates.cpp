d8b4ee14276484a11acb3855a76d9e7e6beec545
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 17:49:20 +0500

    Update 64.overloading_function_templates.cpp

--- 64.overloading_function_templates.cpp
+++ 64.overloading_function_templates.cpp
@@ -1,27 +1,33 @@
+/*
+    This program demonstrates a function template in C++.
+
+    - The template function "maximum" works with any data type:
+        int, double, char, etc.
+    - It takes two values and returns the larger one.
+    - Using templates avoids writing multiple functions for different types.
+*/
+
 #include <iostream>
 using namespace std;
 
-// Function template 1: takes two parameters
+// Function template to find maximum of two values
 template <typename T>
-T add(T a, T b) {
-    return a + b;
-}
-
-// Function template 2: takes three parameters (overloaded)
-template <typename T>
-T add(T a, T b, T c) {
-    return a + b + c;
-}
-
-// Non-template function with same name (also allowed)
-int add(int a, int b, int c, int d) {
-    return a + b + c + d;
+T maximum(T a, T b) {
+    return (a > b) ? a : b;  // return the greater value
 }
 
 int main() {
-    cout << "Add two integers: " << add(5, 10) << endl;         // calls template 1
-    cout << "Add three doubles: " << add(1.2, 3.4, 5.6) << endl; // calls template 2
-    cout << "Add four integers: " << add(1, 2, 3, 4) << endl;   // calls non-template function
+    // Integer comparison
+    int x = 10, y = 20;
+    cout << "Maximum of " << x << " and " << y << " is " << maximum(x, y) << endl;
+
+    // Double comparison
+    double p = 3.5, q = 2.7;
+    cout << "Maximum of " << p << " and " << q << " is " << maximum(p, q) << endl;
+
+    // Character comparison
+    char c1 = 'A', c2 = 'Z';
+    cout << "Maximum of " << c1 << " and " << c2 << " is " << maximum(c1, c2) << endl;
 
     return 0;
 }
