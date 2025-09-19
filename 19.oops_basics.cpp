1c2b61c3ad2ec3d57d84afe24b05ba65a8af1bce
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:37:20 +0500

    Update 19.oops_basics.cpp

--- 19.oops_basics.cpp
+++ 19.oops_basics.cpp
@@ -1,38 +1,49 @@
+// This program demonstrates basic Object-Oriented Programming (OOP) concepts in C++:
+// 1. Class and objects
+// 2. Encapsulation (private members)
+// 3. Constructor
+// 4. Destructor
+// 5. Methods
+
 #include <iostream>
 using namespace std;
 
-// Class definition
+// =========================
+// Class Definition
+// =========================
 class Student {
 private:
-    int id;         // encapsulation: private data
-    string name;
+    int id;         // private variable, cannot be accessed directly outside class
+    string name;    // private variable
 
 public:
-    // Constructor
+    // Constructor: called automatically when object is created
     Student(int i, string n) {
         id = i;
         name = n;
         cout << "Constructor called for " << name << endl;
     }
 
-    // Method to display details
+    // Method to display student details
     void display() {
         cout << "ID: " << id << ", Name: " << name << endl;
     }
 
-    // Destructor
+    // Destructor: called automatically when object goes out of scope
     ~Student() {
         cout << "Destructor called for " << name << endl;
     }
 };
 
 int main() {
-    // Object creation
+
+    // Creating objects of Student class
     Student s1(1, "Umar");
     Student s2(2, "Ali");
 
+    // Calling methods
     s1.display();
     s2.display();
 
-    return 0;
+    return 0; // end of program
 }
