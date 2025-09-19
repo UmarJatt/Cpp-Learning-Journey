86da95fb549b2107e8290c8583dafa8db4332228
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 17:37:36 +0500

    Update 50.this_pointer.cpp

--- 50.this_pointer.cpp
+++ 50.this_pointer.cpp
@@ -1,3 +1,13 @@
+/*
+    This program demonstrates how to use the "this" pointer in C++:
+
+    1. The Student class has a constructor and member functions that use "this" pointer 
+       to refer to the current object.
+    2. "this->" resolves ambiguity when parameter names match member variable names.
+    3. A member function returns the current object using "return *this" to allow 
+       chaining of function calls.
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -6,36 +16,36 @@ class Student {
     int age;
 
 public:
+    // Constructor: using 'this' pointer to set member variables
     Student(string name, int age) {
-        // Resolving ambiguity using "this" pointer
         this->name = name;
         this->age = age;
     }
 
+    // Function to set data, using 'this' pointer
     void setData(string name, int age) {
-        // "this" refers to the calling object
         this->name = name;
         this->age = age;
     }
 
+    // Function to display student details
     void display() {
         cout << "Name: " << this->name << ", Age: " << this->age << endl;
     }
 
-    // Returning current object using this pointer
+    // Function to update age and return the current object to allow chaining
     Student& updateAge(int newAge) {
         this->age = newAge;
-        return *this;  // returning object itself
+        return *this;  // return the object itself
     }
 };
 
 int main() {
-    // Creating object
+    // Creating an object of Student
     Student s1("Umar", 19);
-
     s1.display();
 
-    // Using setData (uses this-> internally)
+    // Using setData to update values
     s1.setData("Ali", 20);
     s1.display();
 
