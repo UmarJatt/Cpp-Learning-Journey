bade2838c1d78d9bcfff9ea1cebc516ab6a90e34
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:42:04 +0500

    Update 25.friend_functions.cpp

--- 25.friend_functions.cpp
+++ 25.friend_functions.cpp
@@ -1,31 +1,45 @@
+// This program demonstrates:
+// 1. Creating a class with private members
+// 2. Using a friend function to access private data
+// 3. Calculating area of a box
+
 #include <iostream>
 using namespace std;
 
+// =========================
+// Class Definition
+// =========================
 class Box {
 private:
-    int length;
-    int width;
+    int length;  // private member
+    int width;   // private member
 
 public:
+    // Constructor to initialize Box dimensions
     Box(int l, int w) {
         length = l;
         width = w;
     }
 
-    // Declaring friend function
+    // Friend function declaration
     friend int calculateArea(Box b);
 };
 
-// Friend function (not a member, but has access to private data)
+// =========================
+// Friend Function
+// =========================
+// This function is not a member of the class, 
+// but it can access private members of Box
 int calculateArea(Box b) {
     return b.length * b.width;
 }
 
 int main() {
+    // Create Box object
     Box b1(10, 5);
 
-    // Friend function can access private members
+    // Use friend function to calculate area
     cout << "Area of Box: " << calculateArea(b1) << endl;
 
-    return 0;
+    return 0; // end of program
 }
