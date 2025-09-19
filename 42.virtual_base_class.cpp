6d7e2e8b988ff4e373d85b9064553d3024e5c606
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:58:42 +0500

    Update 42.virtual_base_class.cpp

--- 42.virtual_base_class.cpp
+++ 42.virtual_base_class.cpp
@@ -1,3 +1,28 @@
+/*
+This program demonstrates **virtual inheritance** to solve the diamond problem:
+
+1. Base class: Person
+   - Method: show() prints "I am a Person"
+
+2. Derived class 1: Student
+   - Inherits **virtually** from Person
+   - Method: study()
+
+3. Derived class 2: Teacher
+   - Inherits **virtually** from Person
+   - Method: teach()
+
+4. Derived class 3: Assistant
+   - Inherits from both Student and Teacher
+   - Method: work()
+   - No ambiguity occurs for show() because Student and Teacher inherit Person virtually
+
+5. Main function:
+   - Create Assistant object
+   - Call show(), study(), teach(), work()
+   - Only **one copy of Person** exists due to virtual inheritance
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -35,7 +60,7 @@ public:
 
 int main() {
     Assistant a;
-    a.show();   // No ambiguity because of virtual base class
+    a.show();   // No ambiguity due to virtual inheritance
     a.study();
     a.teach();
     a.work();
