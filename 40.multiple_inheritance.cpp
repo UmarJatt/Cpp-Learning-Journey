b2c1cf17702c90646f51bf2f335d70a62bc0f578
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:57:17 +0500

    Update 40.multiple_inheritance.cpp

--- 40.multiple_inheritance.cpp
+++ 40.multiple_inheritance.cpp
@@ -1,3 +1,25 @@
+/*
+This program demonstrates **multiple inheritance** in C++:
+
+1. Base class 1: Person
+   - Members: name, age (protected)
+   - Methods: setPerson(), showPerson()
+
+2. Base class 2: Skills
+   - Members: skill (protected)
+   - Methods: setSkill(), showSkill()
+
+3. Derived class: Student (inherits from both Person and Skills)
+   - Adds: id (private)
+   - Methods: setStudent(), showStudent()
+   - Can access protected members of both base classes
+
+4. Main function:
+   - Create a Student object
+   - Set details using setStudent()
+   - Display all details using showStudent()
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -34,14 +56,14 @@ class Student : public Person, public Skills {
     int id;
 public:
     void setStudent(string n, int a, int i, string s) {
-        setPerson(n, a);
-        setSkill(s);
-        id = i;
+        setPerson(n, a);  // Set base class 1 members
+        setSkill(s);      // Set base class 2 members
+        id = i;           // Set derived class member
     }
     void showStudent() {
-        showPerson();
-        showSkill();
-        cout << "ID: " << id << endl;
+        showPerson();     // Display Person info
+        showSkill();      // Display Skills info
+        cout << "ID: " << id << endl; // Display derived class info
     }
 };
 
