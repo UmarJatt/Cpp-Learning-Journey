62e4f0e6bb0922671c96fa01639e71315a55d848
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 16:36:38 +0500

    Update 18.function_overloading.cpp

--- 18.function_overloading.cpp
+++ 18.function_overloading.cpp
@@ -1,25 +1,35 @@
+// This program demonstrates function overloading in C++
+// Function overloading allows multiple functions with the same name
+// but different parameter types or number of parameters
+
 #include <iostream>
 using namespace std;
 
-// Function to add two integers
+// =========================
+// Function Definitions
+// =========================
+
+// Add two integers
 int add(int a, int b) {
     return a + b;
 }
 
-// Function to add two doubles
+// Add two double numbers
 double add(double a, double b) {
     return a + b;
 }
 
-// Function to add three integers
+// Add three integers
 int add(int a, int b, int c) {
     return a + b + c;
 }
 
 int main() {
+
+    // Using function overloading
     cout << "Add two integers 5 + 10 = " << add(5, 10) << endl;
     cout << "Add two doubles 2.5 + 3.7 = " << add(2.5, 3.7) << endl;
     cout << "Add three integers 1 + 2 + 3 = " << add(1, 2, 3) << endl;
 
-    return 0;
+    return 0; // end of program
 }
