f2d39c26ffc948c23cf2bcd83eeff8e277fc61c1
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-19 17:01:27 +0500

    Update 44.constructors_in_derived_class.cpp

--- 44.constructors_in_derived_class.cpp
+++ 44.constructors_in_derived_class.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 using namespace std;
 
-// Base class
+/*
+This program demonstrates **constructor inheritance** in C++.
+
+1. Base class: Person
+   - Members: name, age
+   - Constructor initializes name and age
+   - Method: showPerson() to display base class info
+
+2. Derived class: Student
+   - Inherits from Person
+   - Member: id
+   - Constructor calls **base class constructor** using initializer list
+   - Method: showStudent() displays base + derived info
+
+3. Main function:
+   - Create Student object
+   - Display its details
+*/
+
 class Person {
 protected:
     string name;
     int age;
 public:
-    Person(string n, int a) {   // Base constructor
+    // Base class constructor
+    Person(string n, int a) {
         name = n;
         age = a;
         cout << "Person constructor called" << endl;
     }
+
     void showPerson() {
         cout << "Name: " << name << ", Age: " << age << endl;
     }
@@ -21,19 +41,22 @@ public:
 class Student : public Person {
     int id;
 public:
-    Student(string n, int a, int i) : Person(n, a) {  // Call base constructor
+    // Derived constructor calls base constructor using initializer list
+    Student(string n, int a, int i) : Person(n, a) {
         id = i;
         cout << "Student constructor called" << endl;
     }
+
     void showStudent() {
-        showPerson();
+        showPerson();   // Call base class method
         cout << "ID: " << id << endl;
     }
 };
 
 int main() {
+    // Create Student object
     Student s("Umar", 19, 101);
-    s.showStudent();
+    s.showStudent();    // Display all info
 
     return 0;
 }
